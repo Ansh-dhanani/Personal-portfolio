e3a42d70d54ec25af2c485623d386a0296a07839
Ansh-Dhanani <dhananiansh01@gmail.com>
2025-09-13 18:26:37 +0530

    analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { AuthProvider, useAuth } from "./context/AuthContext";
 import Home from "./pages/Home";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
+import { Analytics } from "@vercel/analytics/next"
 
 const PrivateRoute = ({ children }) => {
   const { token } = useAuth();
@@ -26,6 +27,7 @@ const App = () => {
                 </PrivateRoute>
               } 
             />
+             <Analytics />
           </Routes>
         </ThemeProvider>
       </Router>
