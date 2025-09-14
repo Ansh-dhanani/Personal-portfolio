b9db808750fe4c6b12ed1c1ca8cd98b1360295db
Ansh-Dhanani <dhananiansh01@gmail.com>
2025-09-14 16:55:44 +0530

    analytics removed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { AuthProvider, useAuth } from "./context/AuthContext";
 import Home from "./pages/Home";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
-import { Analytics } from "@vercel/analytics/react"
+
 
 const PrivateRoute = ({ children }) => {
   const { token } = useAuth();
@@ -30,7 +30,7 @@ const App = () => {
           </Routes>
         </ThemeProvider>
       </Router>
-      <Analytics />
+
     </AuthProvider>
   );
 };
